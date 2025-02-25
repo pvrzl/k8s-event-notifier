b3ca16c0562a2318bf450a41e5ac2ab0275f3c83
ef <5966574+pvrzl@users.noreply.github.com>
2025-02-25 21:58:23 +0700

    adjust the type

--- api/v1/notifier_types.go
+++ api/v1/notifier_types.go
@@ -20,22 +20,68 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+type Channel string
+
+const (
+	Slack Channel = "slack"
+)
+
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // NotifierSpec defines the desired state of Notifier.
 type NotifierSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Channel to use
+	// +kubebuilder:validation:Enum=slack
+	Channel Channel `json:"channel"`
 
-	// Foo is an example field of Notifier. Edit notifier_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Namespaces to monitor for events
+	// +kubebuilder:validation:MinItems=1
+	Namespaces []string `json:"namespaces"`
+
+	// Event types to notify on (e.g., Warning, Normal)
+	// +kubebuilder:validation:MinItems=1
+	EventTypes []string `json:"eventTypes"`
+
+	// Event reasons to filter notifications (e.g., ImagePullFailed, CrashLoopBackOff)
+	// +optional
+	EventReasons []string `json:"eventReasons,omitempty"`
+
+	// Target webhook URL
+	// +kubebuilder:validation:Pattern=`^https?://.+`
+	Webhook string `json:"webhook"`
+
+	// Default settings to apply if not provided
+	// +optional
+	DefaultSettings *NotifierDefaults `json:"defaultSettings,omitempty"`
+}
+
+// NotifierDefaults defines optional default settings for notification formatting
+type NotifierDefaults struct {
+	// Prefix for messages (e.g., "[K8s Alert]")
+	// +optional
+	MessagePrefix string `json:"messagePrefix,omitempty"`
+
+	// Enable detailed logging of events in Slack messages
+	// +optional
+	EnableVerbose bool `json:"enableVerbose,omitempty"`
 }
 
 // NotifierStatus defines the observed state of Notifier.
 type NotifierStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Current observed generation
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// Last event processed timestamp
+	// +optional
+	LastEventTime *metav1.Time `json:"lastEventTime,omitempty"`
+
+	// List of last processed events for debugging
+	// +optional
+	RecentEvents []string `json:"recentEvents,omitempty"`
+
+	// Status message
+	// +optional
+	StatusMessage string `json:"statusMessage,omitempty"`
 }
 
 // +kubebuilder:object:root=true
